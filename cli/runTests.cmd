6bba6c8b2d4e466838a8b664817b2b86faca8a7c
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-03-08 06:20:25 +0100

    Prepare core release

--- cli/runTests.cmd
+++ cli/runTests.cmd
@@ -1,3 +1,3 @@
 cd test
-galen check specs/bootstrap.gspec --url "http://getbootstrap2.com" --size "640x480" --htmlreport ../../../reports/
-galen test galen.test -DwebsiteUrl="http://getbootstrap2.com"
+galen check specs/bootstrap.gspec --url "http://getbootstrap.com" --size "640x480" --htmlreport ../../../reports/
+galen test galen.test -DwebsiteUrl="http://getbootstrap.com"
