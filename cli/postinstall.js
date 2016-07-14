3c7c3726753f49fbb8a7dbf88e182d7d89c3491f
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-07-14 11:04:07 +0200

    Adding config commandline flag and autocompletio (#22)

--- cli/postinstall.js
+++ cli/postinstall.js
@@ -6,8 +6,8 @@ if (process.platform === 'darwin') {
 }
 if (process.platform === 'linux') {
   console.info('\n\nTo \x1b[4menable CLI tab autocompletion\x1b[0m run: \n' +
-    ' \x1b[7mgalen completion >> ~/.~/.bashrc\x1b[0m \n' +
+    ' \x1b[7mgalen completion >> ~/.bashrc\x1b[0m \n' +
     'or\n' +
-    ' \x1b[7mgalen completion >> ~/.~/.zshrc\x1b[0m ' +
+    ' \x1b[7mgalen completion >> .~/.zshrc\x1b[0m ' +
     '\n\n');
 }
