55b9eae86bb85200ece49696f1c97bbb4285b0b0
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-04-03 04:36:41 +0200

    chore(build): Fix travis build

--- cli/test/tests.js
+++ cli/test/tests.js
@@ -3,17 +3,17 @@
  * functions correctly.
  */
 
-var childProcess = require('child_process')
-var fs = require('fs')
-var path = require('path')
-var helper = require('../lib/helper')
+var childProcess = require('child_process');
+var fs = require('fs');
+var path = require('path');
+var helper = require('../lib/helper');
 
-exports.testCleanPath = function(test) {
-	test.expect(5)
-	test.equal('/Users/hypery2k/bin', helper.cleanPath('/Users/hypery2k/bin:./bin'))
-	test.equal('/Users/hypery2k/bin:/usr/bin', helper.cleanPath('/Users/hypery2k/bin:./bin:/usr/bin'))
-	test.equal('/usr/bin', helper.cleanPath('./bin:/usr/bin'))
-	test.equal('', helper.cleanPath('./bin'))
-	test.equal('/Work/bin:/usr/bin', helper.cleanPath('/Work/bin:/Work/as/node_modules/.bin:/usr/bin'))
-	test.done()
-}
+exports.testCleanPath = function (test) {
+  test.expect(5);
+  test.equal('/Users/hypery2k/bin', helper.cleanPath('/Users/hypery2k/bin:./bin'));
+  test.equal('/Users/hypery2k/bin:/usr/bin', helper.cleanPath('/Users/hypery2k/bin:./bin:/usr/bin'));
+  test.equal('/usr/bin', helper.cleanPath('./bin:/usr/bin'));
+  test.equal('', helper.cleanPath('./bin'));
+  test.equal('/Work/bin:/usr/bin', helper.cleanPath('/Work/bin:/Work/as/node_modules/.bin:/usr/bin'));
+  test.done();
+};
