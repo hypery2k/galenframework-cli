488ff5f8d71fa57c759f87fda91062be7d87ca83
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-06-05 20:36:14 +0200

    chore(build): Fix local test path

--- cli/runTestsLocal.sh
+++ cli/runTestsLocal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ../test
-../node_modules/.bin/galen check specs/bootstrap.gspec --url "http://getbootstrap.com" --size "640x480" --htmlreport ../../../reports/
-../node_modules/.bin/galen test galen.test -DwebsiteUrl="http://getbootstrap.com"
+../tmp/node_modules/.bin/galen check specs/bootstrap.gspec --url "http://getbootstrap.com" --size "640x480" --htmlreport ../../../reports/
+../tmp/node_modules/.bin/galen test galen.test -DwebsiteUrl="http://getbootstrap.com"
