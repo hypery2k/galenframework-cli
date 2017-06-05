5b24b35ab3b1844563b2d11f065a34a1f2d52fcd
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-06-05 20:43:53 +0200

    chore(build): Fix local test script

--- cli/runTestsLocal.sh
+++ cli/runTestsLocal.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd ../test
-../tmp/node_modules/.bin/galen check specs/bootstrap.gspec --url "http://getbootstrap.com" --size "640x480" --htmlreport ../../../reports/
-../tmp/node_modules/.bin/galen test galen.test -DwebsiteUrl="http://getbootstrap.com"
+../tmp/node_modules/.bin/galen version
