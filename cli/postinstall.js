0317629b2f8319eea6f680fbd70c0f9c8875ad81
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-01-02 21:11:24 +0100

    feat(Safari): Drop Safari extentsion installation
    BREAKING CHANGE: No legacy safari  webdriver extension for Selenium

--- cli/postinstall.js
+++ cli/postinstall.js
@@ -86,43 +86,8 @@ return checkVersionDeferred.promise;
 
 return npmconfDeferred.promise;
         })
-        .then(function(conf) {
-            tmpPath = findSuitableTempDirectory(conf);
-            var platform = process.platform;
-            // offer safari driver installation
-            if (platform === 'darwin') {
-                var npmconfDeferred = kew.defer();
-                npmconf.load(npmconfDeferred.makeNodeResolver());
-                npmconfDeferred.promise.then(function() {
-                    var downloadUrl = process.env.SAFARIDRIVER_CDNURL ||
-                        'https://selenium-release.storage.googleapis.com/2.48/SafariDriver.safariextz';
-                    var fileName = downloadUrl.split('/').pop();
-                    var downloadedFile = path.join(tmpPath, fileName);
-                    if (!fs.existsSync(downloadedFile)) {
-                        log.info('Downloading', downloadUrl);
-
-return requestBinary(downloadUrl, downloadedFile);
-                    }
-                        log.info('Download already available at', downloadedFile);
-
-return downloadedFile;
-
-                }).then(function(downloadedFile) {
-                    // request to open safari extension installation
-                    var spawn = require('child_process').spawn;
-                    log.info('Opening file ', downloadedFile);
-                    spawn('open', [downloadedFile], {
-                        'detached': true
-                    });
-                    exit(0);
-                })
-.fail(function(err) {
-                    log.error('Safari Driver installation failed', err, err.stack);
-                    exit(1);
-                });
-            } else {
-                exit(0);
-            }
+        .then(function() {
+          exit(0);
         })
 }
 
